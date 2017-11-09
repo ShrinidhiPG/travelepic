75a8b75db3bf2abae6573db13f58f92c3517e818
ShrinidhiPG <shrinidhipg96@gmail.com>
2017-11-09 18:56:32 +0530

    Update placeSearch.js

--- js/placeSearch.js
+++ js/placeSearch.js
@@ -5,7 +5,7 @@ function searchPetrolBunks()
 	var request = {
 		location: latlng,
 		radius: '5000',
-		types: ['store','gas_station']
+		types: ['parking']
 	};
 
 	service = new google.maps.places.PlacesService(map);
@@ -18,9 +18,24 @@ function placeMarkers(results,status)
 	    for (var i = 0; i < results.length; i++) {
 	      var place = results[i];
 	      console.log(results[i]);
-	      //createMarkers(place);
+	      createMarkers(place);
 	    }
 	}
 	else
 		alert(status)
+}
+
+function createMarkers(place){
+	var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
+        var icons = {
+          parking: {
+            icon: iconBase + 'parking_lot_maps.png',
+          }
+        };
+		
+	var marker = new google.maps.Marker({
+      map: map,
+	  icon: icons[place.types[0]].icon,
+      position: place.geometry.location
+    });
 }
